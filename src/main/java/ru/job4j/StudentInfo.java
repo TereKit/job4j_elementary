1e31d744e4e9b6726e514b4fdf64c0945bf92264
Екатерина <kitova@list.ru>
2022-11-07 08:52:16 +0500

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Terenteva Ekaterina");
+        System.out.println("08.06.1989");
+
+    }
+}
